5452ea2469ca4ed4d781e5c829ee14b1d4d21d93
matthew-sun <527551750@qq.com>
2014-08-15 16:54:17 +0800

    add itpl

---
+++ labs/itpl/itpl.js
@@ -0,0 +1,11 @@
+/**
+ * itpl(爱模板) ———— javascirpt前端模板引擎
+ * @name itpl
+ * @description 提供不含js逻辑的前端模板解析，提供js预编译(经过编译的模板，输出效率将提升20倍以上)，提供直接模板方法
+ * @author matthewsun
+ * @link matthew-sun@foxmail.com
+ */
+
+define(function(require, exports, module){
+    
+})
